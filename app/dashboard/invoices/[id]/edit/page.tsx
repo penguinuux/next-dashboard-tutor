27d35736a355aa71404d9bdc6ddcc73600e149aa
Pablo Adriano <pabloadrianomc@gmail.com>
2023-11-18 14:19:32 -0300

    feat: improve website SEO
    Add the pages metadata.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,26 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata, ResolvingMetadata } from 'next';
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const id = params.id;
+  const invoice = await fetchInvoiceById(id);
+  const customer = await fetchCustomers();
+  const name = customer.find((c) => c.id === invoice?.customer_id)?.name;
+
+  return {
+    title: `Edit ${name} invoice`,
+  };
+}
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
